544323df9674a6335eca6bc47b707d0b00561245
Reed Williams <Reedwilliams8404@gmail.com>
2015-02-25 01:29:02 -0600

    subclass

---
+++ Developer.java
@@ -0,0 +1,18 @@
+package monDepExpensesApp;
+
+/**
+ * Developers warrant a monthly allocation of $1000.00, per Developer.
+ * 
+ * Developer extends Employee.
+ * 
+ * @author Reed
+ */
+public class Developer extends Employee {
+	
+	/**
+	 * Developer() creates a Developer, who is paid $1000.0 a month.
+	 */
+	public Developer() {
+		expenses = 1000.0;
+	}
+}
