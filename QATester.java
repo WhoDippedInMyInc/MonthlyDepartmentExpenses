e02437d6d9089cd971518e74cef620bdd17cd4ce
Reed Williams <Reedwilliams8404@gmail.com>
2015-02-25 01:29:12 -0600

    subclass

---
+++ QATester.java
@@ -0,0 +1,18 @@
+package monDepExpensesApp;
+
+/**
+ * QA Testers warrant a monthly allocation of $500.00, per QA Tester.
+ * 
+ * QATester extends Employee.
+ * 
+ * @author Reed
+ */
+public class QATester extends Employee {
+	
+	/**
+	 * QATester() creates a QA Tester, who is paid $500.0 a month.
+	 */
+	public QATester() {
+		expenses = 500.0;
+	}
+}
